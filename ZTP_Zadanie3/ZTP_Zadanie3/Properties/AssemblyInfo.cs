e4bbdc96025b0bedaeceb4442ae5a2eeb286c21e
Karolina Raczkowska <karolina.raczkowska1@gmail.com>
2015-04-03 09:53:29 +0200

    v2

--- ZTP_Zadanie3/ZTP_Zadanie3/Properties/AssemblyInfo.cs
+++ ZTP_Zadanie3/ZTP_Zadanie3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ZTP_Zadanie3")]
+[assembly: AssemblyTitle("Chain")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ZTP_Zadanie3")]
+[assembly: AssemblyProduct("Chain")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
